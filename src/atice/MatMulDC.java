4c61181576410e8de1ba1647fe91ac6832df1c96
umaram <umaram@supercom>
2013-12-13 22:52:23 +0530

    finally !!! matrix multiplication successfully done

--- src/atice/MatMulDC.java
+++ src/atice/MatMulDC.java
@@ -8,13 +8,15 @@ package atice;
  *
  * @author umaram
  */
-public class MAddDivCon {
+public class MatMulDC {
     int odi=0;
     public static void main(String[] args){
-        MAddDivCon m1=new MAddDivCon();
+        MatMulDC m1=new MatMulDC();
         //m1.Td2Od(m1.A);
         //m1.Display2D(m1.A);
-        m1.SqMatMulDC(m1.A, m1.B);
+        int[][] C=m1.SqMatMulDC(m1.A, m1.B);
+        m1.Display2D(C);
+        
     }
     
     int[][] A={{1,2,3,4},
@@ -34,65 +36,49 @@ public class MAddDivCon {
             int C[][]=new int[A.length][A.length];
             //System.out.println("lenght of A "+A.length);
             //System.out.println("value of new length "+A.length/2);
-            int D[]=Td2Od(A);
             int[][] A11=new int[A.length/2][A.length/2];
-            A11=splitArr(D, A11);
             int[][] A12=new int[A.length/2][A.length/2];
-            A12=splitArr(D, A12);
             int[][] A21=new int[A.length/2][A.length/2];
-            A21=splitArr(D, A21);
             int[][] A22=new int[A.length/2][A.length/2];
-            A22=splitArr(D, A22);
-            odi=0;
-            D=Td2Od(B);
+            A11=splitArr(A, A11,0,0);
+            A12=splitArr(A, A12,0,A12.length);
+            A21=splitArr(A, A21,A21.length,0);            
+            A22=splitArr(A, A22,A22.length,A22.length);
+            
             int[][] B11=new int[A.length/2][A.length/2];
             int[][] B12=new int[A.length/2][A.length/2];
             int[][] B21=new int[A.length/2][A.length/2];
             int[][] B22=new int[A.length/2][A.length/2];
-            B11=splitArr(D, B11);
-            B12=splitArr(D, B12);
-            B21=splitArr(D, B21);
-            B22=splitArr(D, B22);
-            
-            odi=0;
+            B11=splitArr(B, B11,0,0);
+            B12=splitArr(B, B12,0,A12.length);
+            B21=splitArr(B, B21,A21.length,0);            
+            B22=splitArr(B, B22,A22.length,A22.length);
+                        
             int[][] C11=new int[A.length/2][A.length/2];
             int[][] C12=new int[A.length/2][A.length/2];
             int[][] C21=new int[A.length/2][A.length/2];
             int[][] C22=new int[A.length/2][A.length/2];
             C11=mSum(SqMatMulDC(A11, B11), SqMatMulDC(A12, B21));
-            /*D=combiInC(D,C11);
-            C12=mSum(SqMatMulDC(A11, B12), SqMatMulDC(A12, B22));
-            D=combiInC(D,C12);
+            C12=mSum(SqMatMulDC(A11, B12), SqMatMulDC(A12, B22)); 
             C21=mSum(SqMatMulDC(A21, B11), SqMatMulDC(A22, B21));
-            D=combiInC(D,C21);
             C22=mSum(SqMatMulDC(A21, B12), SqMatMulDC(A22, B22));
-            D=combiInC(D,C22);
-            odi=0;
-            C=Od2Td(C.length,D);
-            odi=0;*/
+            Display2D(C11);
+            Display2D(C12);
+            Display2D(C21);
+            Display2D(C22);
+            
+            C=combiInC(C, C11, 0, 0);
+            C=combiInC(C, C12, 0, C12.length);
+            C=combiInC(C, C21, C21.length, 0);
+            C=combiInC(C, C22, C22.length, C22.length);
+            Display2D(C);
+            
             return C;
         }
         
     }
     
-    int[] Td2Od(int[][] A){
-        //System.out.println("lenght of a.length "+A.length);
-        int D1[]=new int[(A.length)*(A.length)];
-        //System.out.println("value of D1 "+D1.length);
-        int Odi=0;
-        int rci=A.length;
-        for(int ri=0;ri<rci;ri++){
-            for(int ci=0;ci<rci;ci++){
-                D1[Odi]=A[ri][ci];
-                Odi++;
-                //System.out.println("value of ci+ri " + (ci+ri));
-            }
-        }
-        
-        //Display(D1);
-        //splitArr(D1);
-        return D1;
-    }
+    
     
     void Display2D(int[][] Arr){
         System.out.println("start of 2d array");
@@ -116,42 +102,44 @@ public class MAddDivCon {
         System.out.println("End of Display");
     }
     
-    int[][] splitArr(int[] A,int B[][]){
-        //Odi=0;
-        int ind=(int)Math.sqrt(A.length/4);
-        //System.out.println("value of ind "+ind);
-        B=new int[ind][ind];
-        for(int r=0;r<ind;r++){
-            for(int c=0;c<ind;c++){
-                B[r][c]=A[odi];
-                odi++;
+    int[][] splitArr(int[][] A,int B[][],int rs,int cs){
+        
+        for(int r=0;r<B.length;r++){
+            for(int c=0;c<B.length;c++){
+                B[r][c]=A[r+rs][c+cs];
             }
         }
-        //Display2D(B);
         return B;
         
     }
     
     int[][] mSum(int A[][],int B[][]){
         int ind=A.length;
+        int[][] C=new int[A.length][A.length];
         for(int r=0;r<ind;r++){
             for(int c=0;c<ind;c++){
-                A[r][c]=A[r][c]+B[r][c];
+                C[r][c]=A[r][c]+B[r][c];
             }
         }
-        Display2D(A);
-        return A;
+        //Display2D(A);
+        return C;
     }
     
-    int[] combiInC(int[] C,int[][] A){
-        int ind=A.length;
+    int[][] combiInC(int[][] C,int[][] PC,int rs,int cs){
+        /*int ind=A.length;
         for(int r=0;r<ind;r++){
             for(int c=0;c<ind;c++){
                 C[odi]=A[r][c];
                 odi++;
             }
         }
-        odi--;
+        odi--;*/
+        for(int r=0;r<PC.length;r++){
+            for(int c=0;c<PC.length;c++){
+                C[r+rs][c+cs]=PC[r][c];
+            }
+        }
+        
         return C;
         
     }
