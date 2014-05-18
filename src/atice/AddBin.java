89be315d46165690d4633798e4b0faa80175655c
sam <sampath.skt@gmail.com>
2014-05-18 10:24:00 +0530

    binary add done

--- src/atice/AddBin.java
+++ src/atice/AddBin.java
@@ -1,12 +1,17 @@
 // prob 2.1-4 pg 23
 public class AddBin
 {
+	static JavaL jl;	
 	public static void main(String[] args)
-	{	
+	{	jl = new JavaL();
 		AddBin ab = new AddBin();
 		DataSamples ds = new DataSamples();
 		int A[] = ds.getRandomArr(10);
 		int B[] = ds.getRandomArr(10);
+		System.out.println("The array A");
+		jl.display(A);
+		System.out.println("The array B");
+		jl.display(B);
 		ab.addAB(A,B);
 	}
 
@@ -16,7 +21,22 @@ public class AddBin
 		{
 			A[i] = A[i] + B[i];
 		}
+		System.out.println("sum of a and b");
+		jl.display(A);
 		binarize(A);
 	}
-	
+	private void binarize(int[] sum)
+	{
+		for(int i = 0; i<sum.length;i++)
+		{	int bin;
+			int p=0;
+			for(bin=0;sum[i]!=0; sum[i]/=2,p++)
+			{
+				bin=(int)((bin)+(sum[i]%2)*Math.pow(10,p));	
+			}
+			sum[i] = bin;
+		}
+		System.out.println("the binary values");
+		jl.display(sum);
+	}
 }
