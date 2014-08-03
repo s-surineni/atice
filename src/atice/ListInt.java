8c0035cf674abf15c89a74daba8be2fb5abeada6
sampath <sampath.skt@gmail.com>
2014-08-03 18:00:26 +0530

    listint done

--- src/atice/ListInt.java
+++ src/atice/ListInt.java
@@ -1,7 +1,14 @@
 class ListInt
 {
-    LinkdList<Integer> listInt;
+    LinkdList<ListInt> listInt;
     int value;
+	ListInt(int val)
+	{
+		value = val;
+	}
+	ListInt()
+	{
+	}
     public static void main(String[] args)
     {
 	ListInt lI = new ListInt();
@@ -10,15 +17,16 @@ class ListInt
 
     void begin()
     {
-		listInt = new LinkdList<>();
-		listInt.insert(listInt,1);
+		listInt = new LinkdList<ListInt>();
+		ListInt tmp = new ListInt(1);
+		listInt.insert(listInt,tmp);
 		display();
 		
     }
 	void display()
 	{
 		System.out.println("in display");
-		for(LinkdList<Integer> tmp = listInt.head;tmp != null ;tmp = tmp.next)
+		for(LinkdList<ListInt> tmp = listInt.head;tmp != null ;tmp = tmp.next)
 		{
 			System.out.println(tmp.obj.value);
 		}
