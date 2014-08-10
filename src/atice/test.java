4ac5de1098685472780dba2edbbedf0880fd903a
sampath <sampath.skt@gmail.com>
2014-08-10 21:23:08 +0530

    code to clear screen found out

--- src/atice/test.java
+++ src/atice/test.java
@@ -2,18 +2,34 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package atice;
+//package atice;
 
 /**
  *
  * @author umaram
  */
+import java.io.IOException;
+
 public class test {
     
+    final static String ESC = "\033[";
 public static void main(String[] args){
     int i = 1;
     i = i<<31;
-    System.out.println(i);
+    //System.out.print(ESC + "2J");
+    //System.out.pril String ANSI_CLS = "\u001b[2J";
+final String ANSI_CLS = "\u001b[2J";
+final String ANSI_HOME = "\u001b[H";
+	            System.out.print(ANSI_CLS + ANSI_HOME);
+		            System.out.flush();//ntln(i);
+    try
+    {
+    Runtime.getRuntime().exec("pwd");
+    }
+    catch(IOException e)
+    {
+	 System.out.println("ecep");
+    }
 }
     
 }
