471877c535002824eeccea0a2381590372f5d089
mc13mt49 <mc13mt49@localhost>
2014-09-13 03:30:06 +0530

    lics length completed

--- src/atice/LongIncSubSeq.java
+++ src/atice/LongIncSubSeq.java
@@ -9,25 +9,35 @@ class LongIncSubSeq
 
      void begin()
      {
+          JavaL jl = new JavaL();
           Scanner sc = new Scanner(System.in);
           System.out.println("Enter no of ele in seq");
-          System.out.println("Please enter the sequence seperated by spaces");
           int seqlen=sc.nextInt();
+          System.out.println("Please enter the sequence seperated by spaces");
           int seq[] = new int[seqlen];
           for(int i = 0;i<seqlen;i++)
           {
                seq[i]=sc.nextInt();
           }
+          System.out.println("after input");
+          jl.display(seq);
+          int len[]=new int[seqlen];
           for(int i = 0;i<seqlen;i++)
           {
                int max = 0;
-               for(int j = i;j<i;j++)
+               for(int j = i-1;j>=0;j--)
                {
                     if(seq[i]>seq[j])
                     {
-
+                         max =len[j];
+                         break; 
                     }
                }
+               len[i]=++max;
+               System.out.println("each it "+len[i]);
+
+
           }
+          System.out.println("len is " + len[seqlen-1]);
      }
 }
