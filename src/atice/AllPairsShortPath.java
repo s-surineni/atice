848dc88d9c0787d79a55798847fb26e56f8d16aa
sampath <sampath.skt@gmail.com>
2014-09-16 04:32:33 +0530

    all pairs done

---
+++ src/atice/AllPairsShortPath.java
@@ -0,0 +1,58 @@
+class AllPairsShortPath
+{
+     JavaL jl = new JavaL();
+     int nv ;
+     public static void main(String[] a)
+     {
+	  AllPairsShortPath apsp = new AllPairsShortPath();
+	  apsp.begin();
+     }
+
+     void begin()
+     {
+	  int[][] grph = new GraphAM().getGraph();
+	  jl.display(grph);
+	  nv = grph.length;
+	  int[][] done = makeAPSP(grph,grph);
+	  jl.display(done);
+     }
+
+     int[][] makeAPSP(int[][] L,int[][] W)
+     {
+	  int max = ((1<<20));
+	  System.out.println("max "+max);
+	  int ver = L.length;
+	  int[][] NL = new int[ver][ver];
+	  for(int i = 0;i<ver;i++)
+	  {
+	       for(int j = 0;j<ver;j++)
+	       {
+		    NL[i][j]=max;
+		    for(int k = 0;k<ver;k++)
+		    {
+			 NL[i][j] = min(NL[i][j],L[i][k]+W[k][j]);
+		    }
+	       }
+	  }
+	  if(nv == 1)
+	  {
+	       jl.display(NL);
+	       return NL;
+	  }
+	  else
+	  {
+	       jl.display(NL);
+	       nv--;
+	       NL = makeAPSP(NL,W);
+	       return NL;
+	  }
+     }
+
+     int min(int a,int b)
+     {
+	  if(a>b)
+	       return b;
+
+	  return a;
+     }
+}
--- src/atice/GraphAM.java
+++ src/atice/GraphAM.java
@@ -8,10 +8,10 @@ class GraphAM
      public static void main(String[] a)
      {
 	  GraphAM gam = new GraphAM();
-	  gam.begin();
+	  //gam.begin();
      }
 
-     void begin()
+     int[][] getGraph()
      {
 	  System.out.println("Enter the name of the file");
 	  Scanner sc = new Scanner(System.in);
@@ -27,7 +27,7 @@ class GraphAM
 	  int verts = sc.nextInt();
 	  int[][] AdjMat = new int[verts][verts];
 	  
-	  int max = -((1<<31)+1);
+	  int max = ((1<<20));
 	  for(int i = 0;i<verts;i++)
 	  {
 
@@ -36,16 +36,20 @@ class GraphAM
 		   AdjMat[i][j]= max; 
 	       }
 	  }
+	  for(int j = 0;j<verts;j++)
+	  {
+	       AdjMat[j][j]= 0; 
+	  }
+
 	  for(int i = 0;i<verts;i++)
 	  {
 	       for(int j = sc.nextInt();j>0;j--)
 	       {
 		    int col = sc.nextInt();
 		    AdjMat[i][col] = sc.nextInt();
-		    System.out.print(AdjMat[i][col]+" ");
 	       }
-	       System.out.println();
 	  }
-	  jl.display(AdjMat);
+	  //jl.display(AdjMat);
+	  return AdjMat;
      }
 }
