fbd6bb989f98a4da3d5618a69d3e6accb30d159b
sampath <sampath.skt@gmail.com>
2014-09-22 18:34:52 +0530

    ICPC done

--- src/compes/ACMICPC.java
+++ src/compes/ACMICPC.java
@@ -1,4 +1,4 @@
-package compes
+package compes;
 
 import java.util.Scanner;
 
@@ -15,7 +15,7 @@ class ACMICPC
 	  Scanner sc = new Scanner(System.in);
 	  int[] ppl = new int[sc.nextInt()];
 	  int sub = sc.nextInt();
-	   for(int i = 0;i<ppl.length;i++)
+	  for(int i = 0;i<ppl.length;i++)
 	  {
 	       ppl[i]=sc.nextInt();
 	  }
@@ -26,29 +26,50 @@ class ACMICPC
 	  {
 	       for(int j = i+1;j<ppl.length;j++)
 	       {
+		    //System.out.println("i now "+ppl[i]);
+		    //System.out.println("j now "+ppl[j]);
 		    int ones = 0;
 		    int curr = ppl[i]|ppl[j];
+		    //System.out.println("sum now "+curr);
+
 		    for(int k = 0;k<sub;k++)
 		    {
-			ones = ones+((ppl>>k)%2); 
+			 ones = ones+((curr>>k)%2); 
 		    }
+
+		    //System.out.println("ones now "+ones);
 		    if(max<ones)
 		    {
 			 max = ones;
 			 team=0;
 		    }
-		    team++;
+		    if(max==ones)
+			 team++;
+		    //System.out.println("max now "+max);
+		    //System.out.println("team now "+team);
+		    //System.out.println();
+
 	       }
 	  }
 	  System.out.println(max);
 	  System.out.println(team);
 
      }
-     decToBin(int[] ppl,int sub)
+     void decToBin(int[] ppl,int sub)
      {
-	  for(int i = 0;i<sub;i++)
+	  for(int i = 0;i<ppl.length;i++)
 	  {
-	       
+	       int binNum = ppl[i];
+	       int rem = 0;
+	       int decNum = 0;
+	       for(int j=0;j<sub;j++)
+	       {
+		    rem = binNum%10;
+		    binNum/=10;
+		    decNum+=rem*(Math.pow(2,j));
+
+	       }
+	       ppl[i]=decNum;
 	  }
      }
 }
