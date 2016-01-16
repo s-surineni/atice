ed07e587fe567919c96e58ff7d749fbfd441b7b9
sampath kumar <sampath.skt@gmail.com>
2016-01-15 19:58:29 -0800

    basic decorator done

--- python/deco.py
+++ python/deco.py
@@ -1,7 +1,15 @@
 def outer(aFun):
+	print 'this must be printed'
 	def inner():
-		return aFun() + 1
+		print 'lemme print here'
+		ret = aFun() + 1
+		print 'the value returned is ' + str(ret)
+		return ret
 	return inner
 	
 def aFun():
 	return 1
+	
+aFun = outer(aFun)
+
+aFun()
