75800ffdc6b8e31c3efd0133d31138b81b462ea0
ssurinen <sampath.surineni@oracle.com>
2017-04-10 21:54:52 +0530

    started linked list

---
+++ python/linked_list.py
@@ -0,0 +1,34 @@
+class ChainNode():
+    def __init__(self, ele, nxt):
+        self.ele = ele
+        self.nxt = nxt
+
+ChainNode(None, None)
+
+
+class Chain():
+
+    def __init__(self):
+        self.first_node = None
+        self.listSize = 0
+
+    def empty():
+        return list_size == 0
+
+    def size():
+        return list_size
+
+    def get(index):
+        pass
+
+    def index_of(ele):
+        return None
+
+    def erase(index):
+        pass
+
+    def insert(index, ele):
+        pass
+
+    def output():
+        pass
--- src/atice/Test.java
+++
@@ -1,8 +0,0 @@
-class Test
-{
-     public static void main(String[] args)
-     {
-	  int i = 'a';
-	  System.out.println(i);
-     }
-}
