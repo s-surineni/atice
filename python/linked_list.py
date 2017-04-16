baacb0c7c62b93ff2ceb5f92c63722e0f9416a73
ssurinen <sampath.surineni@oracle.com>
2017-04-16 08:41:19 +0530

    till now

--- python/linked_list.py
+++ python/linked_list.py
@@ -10,7 +10,29 @@ class Chain():
 
     def __init__(self):
         self.first_node = None
-        self.listSize = 0
+        self.list_size = 0
+
+    def __init__(self, source_list):
+        self.list_size = source_list.list_size
+
+        if self.list_size == 0:
+            self.first_node = None
+            return
+
+        src_node = source_list.first_node
+        self.first_node = ChainNode(src_node.ele, None)
+        src_node = src_node.next
+        target_node = self.first_node
+
+        while ! src_node:
+            target_node.nxt = ChainNode(src_node.ele, None)
+            target_node = target_node.nxt
+            src_node = src_node.nxt
+
+        target_node.nxt = None
+
+    def check_index(self, idx):
+        return 0 <= idx <= self.list_size
 
     def empty():
         return list_size == 0
@@ -18,13 +40,34 @@ class Chain():
     def size():
         return list_size
 
-    def get(index):
-        pass
+    def get(self, index):
+        if check_index:
+            current_node = self.first_node
+            idx = 0
 
-    def index_of(ele):
-        return None
+            while idx != index:
+                current_node = current_node.nxt
+                idx += 1
 
-    def erase(index):
+            return current_node.ele
+
+    def index_of(self, ele):
+        current_node = this.first_node
+        idx = 0
+
+        while current_node and current_node.ele != ele:
+            current_node = current_node.nxt
+            idx += 1
+            
+        if current_node:
+            return idx
+        else:
+            return None
+
+    def erase(self, index):
+        if check_index(index):
+            if index == 0:
+                self.first_node = self.first_node.nxt
         pass
 
     def insert(index, ele):
