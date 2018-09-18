1a4857d0495ea92bad00fea74a86fa109c71aaa1
sampath <sampath.surineni@oracle.com>
2018-09-18 08:47:22 +0530

    added min heap implementation

--- ppython/heap.py
+++ ppython/heap.py
@@ -7,9 +7,7 @@ class MaxHeap:
     def max_heapify(self, i):
         l_c_i = (2 * i) + 1
         r_c_i = (2 * i) + 2
-        print('i', i)
-        print('l_c_i', 'r_c_i', l_c_i, r_c_i)
-
+        # here we are using another variable to hold the largest of two, this is one more way to compare largest of three
         if l_c_i < self.heap_size and self.arr[i] < self.arr[l_c_i]:
             largest_i = l_c_i
         else:
@@ -36,3 +34,30 @@ if __name__ == '__main__':
     max_heap = MaxHeap([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
     max_heap.build_max_heap()
     print(max_heap)
+
+
+class MinHeap:
+    def __init__(self, arr):
+        self.heap_size = len(arr)
+        self.arr = arr
+
+    def min_heapify(self, i):
+        l_c_i = (2 * i) + 1
+        r_c_i = (2 * i) + 2
+
+        if l_c_i < self.heap_size and self.arr[i] > self.arr[l_c_i]:
+            smallest = l_c_i
+        else:
+            smallest = i
+        if r_c_i < self.heap_size and self.arr[smallest] > self.arr[r_c_i]:
+            smallest = r_c_i
+        if smallest 1= i:
+            self.arr[smallest_i], self.arr[i] = self.arr[i], self.arr[smallest_i]
+            self.min_heapify(smallest)
+
+    def build_min_heap(self):
+        for idx in range(((self.heap_size // 2) -1), -1, -1):
+            self.min_heapify(idx)
+
+    def __str__(self):
+        return str(self.arr)
