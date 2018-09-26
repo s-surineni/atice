a10ff2ec9c9362e9dde9c158e60c0241a31dede0
sampath surineni <sampath.skt@gmail.com>
2018-09-26 21:30:09 +0530

    started shortes path

---
+++ ppython/min_cost_path2.py
@@ -0,0 +1,10 @@
+# https://practice.geeksforgeeks.org/problems/minimum-cost-path/0
+
+tc = int(input().strip())
+
+for a_tc in range(tc):
+    size = int(input().strip())
+    grid = input().strip().split()
+    grid = [int(i) for i in grid]
+
+    
