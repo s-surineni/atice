0b5ce4483100d01a68b4266886e75f02af1d9011
sampath surineni <sampath.skt@gmail.com>
2018-09-27 21:20:53 +0530

    impleted min_cost_path2

--- ppython/min_cost_path2.py
+++ ppython/min_cost_path2.py
@@ -8,13 +8,16 @@ tc = int(input().strip())
     
 def shortest_path(grid2d, row_size, col_size):
     def is_inside_grid(x, y):
-        if not( x < row_size or y < col_size):
+        if (x >= row_size or y >= col_size or x < 0 or y < 0):
             return False
+        else:
+            return True
 
     dist_matrix = []
 
     for a_row in range(row_size):
         dist_matrix.append([sys.maxsize] * col_size)
+    dist_matrix[0][0] = grid2d[0][0]
     print(dist_matrix)
 
     dr = [-1, 0, 1, 0]
@@ -30,6 +33,9 @@ def shortest_path(grid2d, row_size, col_size):
                 min_dist_ind = trk3
 
         closest_cell = path_cells.pop(trk3)
+        print('*' * 80)
+        print('closest_cell', closest_cell)
+
 
         for trk in range(4):
             x = closest_cell[0] + dr[trk]
@@ -37,14 +43,22 @@ def shortest_path(grid2d, row_size, col_size):
 
             if not is_inside_grid(x, y):
                 continue
-
+            print('after continue')
+            print('x , y ', x, y)
+            print('sum', dist_matrix[closest_cell[0]][closest_cell[1]] + grid2d[x][y])
             if dist_matrix[x][y] > dist_matrix[closest_cell[0]][closest_cell[1]] + grid2d[x][y]:
+                print('inside distance comparison')
                 if dist_matrix[x][y] != sys.maxsize:
                     for trk2 in range(len(path_cells)):
                         if (path_cells[trk2][0], path_cells[trk2][1]) == (x, y):
                             path_cells[trk2][2] = dist_matrix[closest_cell[0]][closest_cell[1]] + grid2d[x][y]
                             break
-                    dist_matrix[x][y] = dist_matrix[closest_cell[0]][closest_cell[1]] + grid2d[x][y]
+                    else:
+                        path_cells.append([x, y, dist_matrix[x][y]])
+                else:
+                    path_cells.append([x, y, dist_matrix[x][y]])
+
+                dist_matrix[x][y] = dist_matrix[closest_cell[0]][closest_cell[1]] + grid2d[x][y]
     print('dist_matrix')
     print(dist_matrix)
 
