d52808c911686384a8d2ea7ff55267186dec92ac
ssurinen <sampath.surineni@oracle.com>
2018-12-05 20:03:43 +0530

    added notes to delete method

--- ppython/trie.py
+++ ppython/trie.py
@@ -46,11 +46,26 @@ class Trie:
             p_crawl = p_crawl.children[idx]
         return p_crawl and p_crawl.is_end_of_word
 
+    # different possibilities
+    # part of ip_str is sub_string of string in trie
+    # whole ip_str is sub_string of string in trie but is not part of trie
+    # whole ip_str is sub_string of string in trie and also is part of trie
+    # whole ip_str is part of trie and is only one string in that branch
+    # whole ip_str is part of trie and also holds other sub_strings which are part of trie
     def _delete_helper(self, p_node, key, level):
+        # needs to be checked as the input string may not be
+        # contained in trie and also not a sub string of
+        # strings exits in trie
         if p_node:
+            # this condition is because the input string may be part of
+            # a sting in trie but might not be stored in trie
             if level == len(key):
-                if p_node.find_if_leaf_node:
+                # this condition is not necessary as it does not add any
+                # change even if it is removed
+                if p_node.find_if_leaf_node():
                     p_node.is_end_of_word = False
+                # last node will be deleted only when
+                # last char of input is last node of a branch
                 return p_node.find_if_free_node()
             else:
                 index = self._char_to_index(key[level])
@@ -62,6 +77,10 @@ class Trie:
         return False
 
     def delete_key(self, key):
+        # input may be the only string for that branch
+        # it may be part of another string
+        # it may hold another string in it
+        # the input may be substring of a string in trie but is not stored itself
         key_length = len(key)
         if key_length > 0:
             self._delete_helper(self.root, key, 0)
