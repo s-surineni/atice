23f2b7af7a45062ae87914d28b784bbdb271d121
sampath surineni <sampath.skt@gmail.com>
2019-01-26 22:13:00 +0530

    knocked out disjoint set program

---
+++ ppython/graphs/disjoint_set_imporved.py
@@ -0,0 +1,61 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, num_of_v):
+        self.num_of_v = num_of_v
+        self.edges = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.edges[u].append(v)
+
+
+class Subset:
+    def __init__(self, parent, rank):
+        self.parent = parent
+        self.rank = rank
+
+
+def find_rep(subsets, node):
+    if subsets[node].parent != node:
+        subsets[node].parent = find_rep(subsets, subsets[node].parent)
+    return subsets[node].parent
+
+
+def union(subsets, u, v):
+    if subsets[u].rank > subsets[v].rank:
+        subsets[v].parent = u
+    elif subsets[v].rank > subsets[u].rank:
+        subsets[u].parent = v
+    else:
+        subsets[v].parent = u
+        subsets[u].rank += 1
+
+
+def doesGraphContainCycle(graph):
+    subsets = []
+
+    for u in range(graph.num_of_v):
+        subsets.append(Subset(u, 0))
+
+    for u in graph.edges:
+        u_rep = find_rep(subsets, u)
+
+        for v in graph.edges[u]:
+            v_rep = find_rep(subsets, v)
+
+            if u_rep == v_rep:
+                return True
+            else:
+                union(subsets, u_rep, v_rep)
+
+
+g = Graph(3)
+g.add_edge(0, 1)
+g.add_edge(1, 2)
+g.add_edge(0, 2)
+
+if doesGraphContainCycle(g):
+    print('Graph contains cycle')
+else:
+    print('Graph does not contain cycle')
