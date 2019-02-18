8c39880b39cf51d443a6baa6b1581685f4354d13
Sampath Surineni <sampath.surineni@oracle.com>
2019-02-18 09:47:18 +0530

    word break working properly now

--- ppython/algos/word_break.py
+++ ppython/algos/word_break.py
@@ -5,17 +5,18 @@ words = None
 def break_words_util(big_string, res):
     # print('res', res)
     # print('big_string', big_string)
+    print(len(big_string))
     for idx in range(1, len(big_string) + 1):  # + 1 because if only one element is
         # left, range(1, len('m')) returns empty list
         # print('currwrod', big_string[:idx])
         curr_str = big_string[:idx]
         if curr_str in words:
-            res += ' ' + curr_str
-            
+            # res += ' ' + curr_str
             if idx == len(big_string):
+                res += ' ' + curr_str  # I was getting wrong answer when this line was before this conditoin need to check
                 print(res)
                 return
-            break_words_util(big_string[idx:], res)
+            break_words_util(big_string[idx:], res + curr_str + ' ')
 
 
 def break_words(big_string):
