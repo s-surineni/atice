99c5e03318ba7aa98ee7639e6cd7f27abb009889
sampath <sampath.skt@gmail.com>
2019-10-27 08:48:56 +0530

    wrote binary tree adt

--- ppython/goodrich_algo/trees/abstract_binary_tree.py
+++ ppython/goodrich_algo/trees/abstract_binary_tree.py
@@ -1,3 +1,6 @@
+from abstract_binary_tree import Tree
+
+
 class BinaryTree(Tree):
     def get_left_child(self, p):
         raise NotImplementedError('must be implemented by subclass')
@@ -14,3 +17,11 @@ class BinaryTree(Tree):
                 return self.get_right_child(parent)
             else:
                 return self.get_left_child(parent)
+
+    def get_children(self, p):
+        left_child = self.get_left_child(p)
+        if left_child:
+            yield left_child
+        right_child = self.get_right_child(p)
+        if right_child:         # book has is not None, may be to work in situations when element is 0?
+            yield right_child
