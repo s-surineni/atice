2f2c890fa6327c8933f357670d9a43bd745f82b2
sampath <sampath.skt@gmail.com>
2019-12-16 18:23:10 +0530

    added priority queue class

---
+++ ppython/goodrich_algo/priority_queues/abstract_priority_queue.py
@@ -0,0 +1,11 @@
+class PriorityQueueBase:
+    class _Item:
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v
+
+        def __lt__(self, other):
+            return self._key < other._key
+
+    def is_empty(self):
+        return len(self) == 0
