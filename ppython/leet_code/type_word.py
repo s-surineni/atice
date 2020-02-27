78c71f00d21708a3b5f3677cb3fe56be39736834
sampath surineni <sampath.skt@gmail.com>
2020-02-27 21:17:30 +0530

    added code 2

--- ppython/leet_code/type_word.py
+++ ppython/leet_code/type_word.py
@@ -1,33 +1,21 @@
-from functools import lru_cache
-from sys import setrecursionlimit as srl
-srl(10**6)
+def maxCandies(status, candies, keys, containedBoxes, initialBoxes):
+    boxes = set(initialBoxes)
+    bfs = [i for i in boxes if status[i]]
+    for i in bfs:
+        for j in containedBoxes[i]:
+            boxes.add(j)
+            if status[j]:
+                bfs.append(j)
+        for j in keys[i]:
+            if status[j] == 0 and j in boxes:
+                bfs.append(j)
+            status[j] = 1
+    return sum(candies[i] for i in bfs)
 
 
-class Solution(object):
-    def minimumDistance(self, word):
-        A = [list("ABCDEF"), list("GHIJKL"), list("MNOPQR"), list("STUVWX"), list("YZ    ")]
-        R, C = len(A), len(A[0])
-
-        index = {}
-        for r, row in enumerate(A):
-            for c, val in enumerate(row):
-                index[val] = r, c
-
-        def dist(v1, v2):
-            if v1 is None or v2 is None:
-                return 0
-
-            r1,c1 = index[v1]
-            r2,c2 = index[v2]
-            return abs(r1-r2) + abs(c1-c2)
-
-        @lru_cache(None)
-        def dp(i, v1=None, v2=None):
-            if i == len(word):
-                return 0
-
-            # move v1 to i
-            ans1 = dist(v1, word[i]) + dp(i+1, word[i], v2)
-            ans2 = dist(v2, word[i]) + dp(i+1, v1, word[i])
-            return min(ans1, ans2)
-        return dp(0)
+status = [1,0,1,0]
+candies = [7,5,4,100]
+keys = [[],[],[1],[]]
+containedBoxes = [[1,2],[3],[],[]]
+initialBoxes = [0]
+print(maxCandies(status, candies, keys, containedBoxes, initialBoxes))
