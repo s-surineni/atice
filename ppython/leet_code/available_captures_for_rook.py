96ddaf673a79ce729ca317ae6f4a3fa6b1191dd3
sampath surineni <sampath.skt@gmail.com>
2020-03-27 07:43:32 +0530

    added new code

--- ppython/leet_code/available_captures_for_rook.py
+++ ppython/leet_code/available_captures_for_rook.py
@@ -1,3 +1,20 @@
+# Instead of 4 different while loops this worked on a single while loop
+# this is better code <!note> <!better>
+def numRookCaptures(self, board):  # 
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] == 'R':
+                x0, y0 = i, j
+    res = 0
+    for i, j in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+        x, y = x0 + i, y0 + j
+        while 0 <= x < 8 and 0 <= y < 8:
+            if board[x][y] == 'p': res += 1
+            if board[x][y] != '.': break
+            x, y = x + i, y + j
+    return res
+
+
 def find_captures(board, ri, ci):
     pawn_count = 0
     # left side
