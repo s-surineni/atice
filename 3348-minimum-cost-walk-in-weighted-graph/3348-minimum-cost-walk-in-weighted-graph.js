e82234a505f33d4587396b83bfc58fc87353c27c
sampath kumar <1143331+s-surineni@users.noreply.github.com>
2025-03-20 20:03:14 +0530

    Time: 86 ms (15.38%), Space: 114 MB (7.69%) - LeetHub

---
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.js
@@ -0,0 +1,63 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[][]} query
+ * @return {number[]}
+ */
+class UnionFind {
+    constructor(size) {
+        this.parent = [];
+        this.rank = [];
+        for (let i = 0; i < size; i++) {
+            this.parent[i] = i;
+            this.rank[i] = 1;
+        }
+    }
+    find(x) {
+        if (this.parent[x] !== x) {
+            this.parent[x] = this.find(this.parent[x]);
+        }
+        return this.parent[x];
+    }
+    union(x, y) {
+        let rootX = this.find(x);
+        let rootY = this.find(y);
+        if (rootX !== rootY) {
+            if (this.rank[rootX] > this.rank[rootY]) {
+                this.parent[rootY] = rootX;
+            } else if (this.rank[rootX] < this.rank[rootY]) {
+                this.parent[rootX] = rootY;
+            } else {
+                this.parent[rootY] = rootX;
+                this.rank[rootX] += 1;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+var minimumCost = function(n, edges, query) {
+    const unionFind = new UnionFind(n);
+    const minPathCost = new Array(n).fill(-1);
+    for (const edge of edges) {
+        const root1 = unionFind.find(edge[1])
+        const root2 = unionFind.find(edge[0])
+        
+        const union = unionFind.union(edge[0], edge[1]);
+        const root = unionFind.find(edge[0]);
+        if (union) {
+            minPathCost[root] &= minPathCost[root1] & minPathCost[root2];
+        }
+        minPathCost[root] &= edge[2];
+    }
+    const answer = [];
+    for (const q of query) {
+        if (unionFind.find(q[0]) === unionFind.find(q[1])) {
+            answer.push(minPathCost[unionFind.find(q[0])]);
+        } else {
+            answer.push(-1);
+        }
+    }
+    return answer;
+};
